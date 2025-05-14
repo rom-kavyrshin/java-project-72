42918793b7b745ca1d9da49ff2aacce121c6eb9f
Roman2 <rom.kawirshin@gmail.com>
2025-05-14 17:48:11 +0300

    Реализован hello world

---
+++ app/src/main/java/hexlet/code/App.java
@@ -0,0 +1,27 @@
+package hexlet.code;
+
+import hexlet.code.util.NamedRoutes;
+import io.javalin.Javalin;
+import io.javalin.rendering.template.JavalinJte;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class App {
+
+    public static Javalin getApp() {
+
+        var app = Javalin.create(config -> {
+            config.bundledPlugins.enableDevLogging();
+            config.fileRenderer(new JavalinJte());
+        });
+
+        app.get(NamedRoutes.rootPath(), context -> context.result("Hello world"));
+
+        return app;
+    }
+
+    public static void main(String[] args) {
+        Javalin app = getApp();
+        app.start(7070);
+    }
+}
