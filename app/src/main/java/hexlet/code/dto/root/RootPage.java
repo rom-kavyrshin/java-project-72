999f452a39561c891f2556b0e7bd7f25bb5a0342
Roman2 <rom.kawirshin@gmail.com>
2025-06-04 22:11:51 +0300

    Унаследовал RootPage от BasePage

--- app/src/main/java/hexlet/code/dto/root/RootPage.java
+++ app/src/main/java/hexlet/code/dto/root/RootPage.java
@@ -1,9 +1,11 @@
 package hexlet.code.dto.root;
 
+import hexlet.code.dto.base.BasePage;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
-public class RootPage {
+public class RootPage extends BasePage {
+
 }
