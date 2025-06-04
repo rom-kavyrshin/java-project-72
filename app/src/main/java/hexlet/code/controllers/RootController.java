33a433c50b817031eeb5734771eabe60b0467abd
Roman2 <rom.kawirshin@gmail.com>
2025-06-05 01:02:07 +0300

    Реализовано отображение flash сообщения

--- app/src/main/java/hexlet/code/controllers/RootController.java
+++ app/src/main/java/hexlet/code/controllers/RootController.java
@@ -3,12 +3,14 @@ package hexlet.code.controllers;
 import hexlet.code.dto.root.RootPage;
 import io.javalin.http.Context;
 
+import static hexlet.code.util.SessionKeys.SESSION_STORE_FLASH_MESSAGE_KEY;
 import static io.javalin.rendering.template.TemplateUtil.model;
 
 public class RootController {
 
     public static void index(Context context) {
         var rootPage = new RootPage();
+        rootPage.setFlashMessage(context.consumeSessionAttribute(SESSION_STORE_FLASH_MESSAGE_KEY));
         context.render("index.jte", model("page", rootPage));
     }
 }
