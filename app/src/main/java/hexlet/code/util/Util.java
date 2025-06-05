43b820a690e8e2befc2d287bf88322f945ab0b90
Roman2 <rom.kawirshin@gmail.com>
2025-06-06 00:32:24 +0300

    Реализован Util метод для форматирования timestamp

--- app/src/main/java/hexlet/code/util/Util.java
+++ app/src/main/java/hexlet/code/util/Util.java
@@ -3,10 +3,13 @@ package hexlet.code.util;
 import java.net.MalformedURLException;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
 
 public class Util {
 
     private static final String PROTOCOL_DELIMETER = "://";
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd.MM.yy HH:mm:ss");
 
     public static String getEnv(String key, String defaultValue) {
         return System.getenv().getOrDefault(key, defaultValue);
@@ -15,5 +18,9 @@ public class Util {
     public static String shrinkUrl(String urlString) throws URISyntaxException, MalformedURLException {
         var url = new URI(urlString).toURL();
         return url.getProtocol() + PROTOCOL_DELIMETER + url.getHost() + (url.getPort() == -1 ? "" : url.getPort());
+    }
+
+    public static String formatTimestamp(Timestamp timestamp) {
+        return DATE_TIME_FORMATTER.format(timestamp.toLocalDateTime());
     }
 }
