fc4d2b9b102f16a16616e6450c5a62edf701edff
Roman2 <rom.kawirshin@gmail.com>
2025-06-18 05:59:31 +0300

    Изменена схема

--- app/src/main/resources/schema.sql
+++ app/src/main/resources/schema.sql
@@ -5,3 +5,15 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP
 );
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    url_id INT REFERENCES urls (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    status_code INT,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP
+);
--- app/src/main/resources/schema_postgresql.sql
+++ app/src/main/resources/schema_postgresql.sql
@@ -5,3 +5,15 @@ CREATE TABLE urls (
     name VARCHAR(255) NOT NULL,
     created_at TIMESTAMP
 );
+
+DROP TABLE IF EXISTS url_checks;
+
+CREATE TABLE url_checks (
+    id SERIAL PRIMARY KEY,
+    url_id SERIAL REFERENCES urls (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    status_code INTEGER,
+    h1 VARCHAR(255),
+    title VARCHAR(255),
+    description TEXT,
+    created_at TIMESTAMP
+);
