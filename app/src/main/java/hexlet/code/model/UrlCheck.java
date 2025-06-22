4535481a98a5a6daa219914221be1244b8f18a57
Roman2 <rom.kawirshin@gmail.com>
2025-06-23 02:23:35 +0300

    Добавлен класс UrlCheck

---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,32 @@
+package hexlet.code.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+
+@EqualsAndHashCode
+@ToString
+@Getter
+@Setter
+public class UrlCheck {
+    private int id;
+
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private long urlId;
+    private Timestamp createdAt;
+
+    public UrlCheck(int statusCode, String title, String h1, String description, long urlId, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+        this.createdAt = createdAt;
+    }
+}
