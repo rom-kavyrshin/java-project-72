833954c5da3d8c996f40a05ac7e5a46294d8f1e3
Roman2 <rom.kawirshin@gmail.com>
2025-06-23 03:34:24 +0300

    Исправлено dto

--- app/src/main/java/hexlet/code/dto/urls/UrlDetailPage.java
+++ app/src/main/java/hexlet/code/dto/urls/UrlDetailPage.java
@@ -2,11 +2,15 @@ package hexlet.code.dto.urls;
 
 import hexlet.code.dto.base.BasePage;
 import hexlet.code.model.Url;
+import hexlet.code.model.UrlCheck;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.List;
 
 @Getter
 @AllArgsConstructor
 public class UrlDetailPage extends BasePage {
     private final Url url;
+    private final List<UrlCheck> urlChecks;
 }
